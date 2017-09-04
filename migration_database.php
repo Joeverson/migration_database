a5431d0f8f55bc1d67024c0035f9394adf047764
Joeverson Barbosa <joeverson.barbosa@unisigma.com.br>
2017-09-04 12:34:32 -0300

    atualizacao v0.0.1
    Mudança na forma de utilização de pk, agora você pode definir uma ou varias PK's para ao sisteama fazer a migração de dados.
    pk agora pode receber uma string ou um array como parametro.

--- migration_database.php
+++ migration_database.php
@@ -5,24 +5,24 @@
  * Date: 02/08/2017
  * Time: 17:44
  */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 
 /**
  * array de configuração do migrate
  * arr = [
  * "src" => [
- *      user => '',
- *      host => '',
- *      pass => '',
- *      database => ''
+ *      user => "",
+ *      host => "",
+ *      pass => "",
+ *      database => ""
  *  ],
  *  "clone" => [
- *      user => '',
- *      host => '',
- *      pass => '',
- *      database => ''
+ *      user => "",
+ *      host => "",
+ *      pass => "",
+ *      database => ""
  *  ]
  * ]
  **/
@@ -43,7 +43,7 @@ class Migration_Database
         foreach($this->array as $db_origin => $a){
             try{
 
-                $this->conn[$db_origin] = new PDO("mysql:host=".$this->array[$db_origin]['host'].";dbname=".$this->array[$db_origin]['database'], $this->array[$db_origin]['user'], $this->array[$db_origin]['pass'], array(\PDO::ATTR_PERSISTENT => true));
+                $this->conn[$db_origin] = new PDO("mysql:host=".$this->array[$db_origin]["host"].";dbname=".$this->array[$db_origin]["database"], $this->array[$db_origin]["user"], $this->array[$db_origin]["pass"], array(\PDO::ATTR_PERSISTENT => true));
 
             }catch(Exception $e){
                 print $e->getMessage();
@@ -55,7 +55,7 @@ class Migration_Database
     {
         if(is_array($array)) $this->array = $array;
         else{
-            print 'informe o array de configuração';
+            print "informe o array de configuração";
             die();
         }
 
@@ -65,22 +65,22 @@ class Migration_Database
      * aqui é tipo o setup do migrate
      *  array
      * [
-     *      pk =>  '',
-     *      table => ''
+     *      pk =>  "",
+     *      table => ""
      * ]
      **/
     public function migrate($array)
     {
 
-        if(!array_key_exists('pk',$array)){
-            print '<br>Não foi encontrado no array "pk"';
+        if(!array_key_exists("pk",$array)){
+            print "\nNão foi encontrado no array 'pk'";
             die();
-        }else if(!array_key_exists('table',$array)){
-            print '<br>Não foi encontrado no array "table"';
+        }else if(!array_key_exists("table",$array)){
+            print "\nNão foi encontrado no array 'table'";
             die();
-        }else if(isset($array['where'])){
-            if(!is_array($array['where'])){
-                print '<br>Não foi encontrado no array o "where" que é um array onde chave e valor ex. "id"=>"3"';
+        }else if(isset($array["where"])){
+            if(!is_array($array["where"])){
+                print "\nNão foi encontrado no array o \"where\" que é um array onde chave e valor ex. \"id\"=>\"3\"";
                 die();
             }else
                 {
@@ -89,38 +89,38 @@ class Migration_Database
                      * ele vai deixar montado o where para as querys.
                     **/
                     $q_where = [];
-                    foreach($array['where'] as $k=>$v){
-                        $q_where[] = "$k='$v'";
+                    foreach($array["where"] as $k=>$v){
+                        $q_where[] = "$k=\"$v\"";
                     }
 
-                    $array['where'] = implode(" and ", $q_where);
+                    $array["where"] = implode(" and ", $q_where);
                 }
 
         }
 
         //ajeitando as variaveis
-        $this->table = $array['table'];
-        $this->pk= $array['pk'];
-        $this->where = isset($array['where']) ? "where ".$array['where'] : "";
+        $this->table = $array["table"];
+        $this->pk= $array["pk"];
+        $this->where = isset($array["where"]) ? "where ".$array["where"] : "";
 
         //conectando
         $this->conn();
 
         //pegando os dados de src e preparando eles
-        $search_table_src = $this->search_table('src');
+        $search_table_src = $this->search_table("src");
 
-        $columns_src = $search_table_src['colunms'];
-        $data_src = $search_table_src['data'];
+        $columns_src = $search_table_src["colunms"];
+        $data_src = $search_table_src["data"];
 
         //pegando informações do clone
-        $search_table_clone = $this->search_table('clone');
+        $search_table_clone = $this->search_table("clone");
 
-        $columns_clone = $search_table_src['colunms'];
+        $columns_clone = $search_table_src["colunms"];
 
         $this->copy_data($search_table_src);
 
 
-        print "<br><b>Finalizado com sucesso</b>";
+        print "\n<b>Finalizado com sucesso</b>";
 
     }
 
@@ -144,8 +144,8 @@ class Migration_Database
             /**
              * condicional caso seja src e não encontre a tabela ele para toda a execução
              **/
-            if($smtp->errorCode() != '00000' and $origin == 'src'){
-                print 'Tabela de origem não encontrada';
+            if($smtp->errorCode() != "00000" and $origin == "src"){
+                print "Tabela de origem não encontrada";
                 die();
             }
 
@@ -154,7 +154,7 @@ class Migration_Database
              * condicional caso seja o clone que esteja acessando
              * e ele vai e vê se eiste a table caso não ele clona a tabela.
             **/
-            if($smtp->errorCode() != '00000' and $origin == 'clone'){
+            if($smtp->errorCode() != "00000" and $origin == "clone"){
                  //cria a tabela
                 $this->create_table_clone();
             }
@@ -167,7 +167,7 @@ class Migration_Database
             $colnm = $this->conn[$origin]->query("SHOW COLUMNS FROM $this->table")->fetchAll(2);
 
             foreach($colnm as $c){
-                $name_colunm[] = $c['Field'];
+                $name_colunm[] = $c["Field"];
             }
 
 
@@ -190,22 +190,22 @@ class Migration_Database
     private function create_table_clone()
     {
         $fields = [];
-        $its = $this->conn['src']->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'multiplicador' AND TABLE_NAME = 'user'")->fetchAll(2);
+        $its = $this->conn["src"]->query("SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = \"multiplicador\" AND TABLE_NAME = \"user\"")->fetchAll(2);
 
         foreach($its as $it){
-            $fields[] = $it['COLUMN_NAME'] ." ". $it['COLUMN_TYPE'] ." ".($it['IS_NULLABLE']=="NO"?"NOT NULL":"")." ".($it['COLUMN_KEY']=="PRI"?"PRIMARY KEY":"")." ".($it['EXTRA']=="auto_increment"?"AUTO_INCREMENT":"");
+            $fields[] = $it["COLUMN_NAME"] ." ". $it["COLUMN_TYPE"] ." ".($it["IS_NULLABLE"]=="NO"?"NOT NULL":"")." ".($it["COLUMN_KEY"]=="PRI"?"PRIMARY KEY":"")." ".($it["EXTRA"]=="auto_increment"?"AUTO_INCREMENT":"");
         }
 
-        $fields = implode(',', $fields);
+        $fields = implode(",", $fields);
 
-        $smtp = $this->conn['clone']->query("create table $this->table ($fields)");
+        $smtp = $this->conn["clone"]->query("create table $this->table ($fields)");
 
-        if($smtp->errorCode() != '00000'){
-            print 'error ao criar a tabela '.$this->table;
+        if($smtp->errorCode() != "00000"){
+            print "error ao criar a tabela ".$this->table;
             die();
         }
 
-        print "<br><b>Tabela Criada no clone</b> name: $this->table";
+        print "\n<b>Tabela Criada no clone</b> name: $this->table";
 
     }
 
@@ -217,9 +217,9 @@ class Migration_Database
     **/
     private function force_create($src)
     {
-        $this->conn['clone']->query("delete from $this->table where $this->where");
+        $this->conn["clone"]->query("delete from $this->table where $this->where");
 
-        foreach($src['data'] as $s)
+        foreach($src["data"] as $s)
         {
             $this->create_register($s);
         }
@@ -229,12 +229,35 @@ class Migration_Database
     private function copy_data($src)
     {
 
-        foreach($src['data'] as $s)
+        foreach($src["data"] as $s)
         {
             /**
              * TODO -> verificação zuada.. da uma olhada depois
             **/
-            $smtp = $this->conn['clone']->query("select " .$this->pk. " from ".$this->table." where ".$this->pk." = '".$s[$this->pk]."'")->fetch(2);
+            $pk_w = [];
+            $pk_s = [];
+
+            if(is_array($this->pk))
+            {
+                foreach($this->pk as $pk)
+                {
+                    $pk_s[] = $pk;
+                    $pk_w[] = "$pk = \"".$s[$pk]."\"";
+                }
+
+                $pk_s = implode(',', $pk_s);
+                $pk_w = implode(' and ', $pk_w);
+
+            }else
+                {
+                    $pk_s = $this->pk;
+                    $pk_w = "$this->pk = \"".$s[$this->pk]."\"";
+                }
+
+
+
+
+            $smtp = $this->conn["clone"]->query("select $pk_s  from $this->table where $pk_w")->fetch(2);
 
             if($smtp == false)
                 $this->create_register($s);
@@ -242,7 +265,7 @@ class Migration_Database
                 $this->update_register($s);
         }
 
-        print "<br><b>Dados Copiados:</b> Qtd. registros".count($src['data']);
+        print "\n<b>Dados Copiados:</b> Qtd. registros".count($src["data"]);
     }
 
 
@@ -263,17 +286,36 @@ class Migration_Database
             $values[":".$k] = $p;
         }
 
-        $keys = implode(',', $keys);
-        $keys_dot = implode(',', $keys_dot);
+        $keys = implode(",", $keys);
+        $keys_dot = implode(",", $keys_dot);
 
 
         $query = "INSERT INTO $this->table ($keys) VALUES ($keys_dot)";
 
 
-        $stmt = $this->conn['clone']->prepare($query);
+        $stmt = $this->conn["clone"]->prepare($query);
         $stmt->execute($values);
 
-        print "<br><b>Criado</b> o registro na table $this->table, [pk=$this->pk]".$arr[$this->pk];
+
+        /**
+         * printando de duas formas se for um array de pks ou uma só pk
+        **/
+        if(is_array($this->pk))
+        {
+            $pp = [];
+
+            foreach($this->pk as $pks){
+                $pp[] = "[pk=$pks]".$arr[$pks];
+            }
+
+            $pp = implode(',', $pp);
+
+            print "\n<b>Criado</b> o registro na table $this->table, $pp";
+        }
+        else
+        {
+            print "\n<b>Criado</b> o registro na table $this->table, [pk=$this->pk]".$arr[$this->pk];
+        }
 
     }
 
@@ -287,21 +329,49 @@ class Migration_Database
 
         foreach($arr as $k=>$p)
         {
-            if(gettype($p) == 'string')
-                $values[] = "$k='$p'";
+            if(gettype($p) == "string")
+                $values[] = "$k=\"$p\"";
             else
                 $values[] = "$k=$p";
         }
 
-        $velues = implode(',', $values);
+        $velues = implode(",", $values);
+
+        $pk_w = [];
+
+        if(is_array($this->pk))
+        {
+            $pp = [];
+
+            foreach($this->pk as $pks){
+                $pk_w = "$pks = '".$arr[$pks]."'";
+            }
+
+            $pp = implode(',', $pp);
+
+            $pk_w = implode(' and ', $pp);
 
 
-        $query = "UPDATE $this->table SET $velues WHERE $this->pk = ".$arr[$this->pk];
+
+            $print = "\n<b>atualizado</b> o registro na table $this->table, $pp";
+        }
+        else
+        {
+            $print = "\n<b>atualizado</b> o registro na table $this->table, [pk=$this->pk]".$arr[$this->pk];
+
+            $pk_w = "$this->pk = '".$arr[$this->pk]."'";
+        }
+
+        $query = "UPDATE $this->table SET $velues WHERE $pk_w";
 
 
-        $stmt = $this->conn['clone']->query($query);
+        $stmt = $this->conn["clone"]->query($query);
 
-        print "<br><b>atualizado</b> o registro na table $this->table, [pk=$this->pk]".$arr[$this->pk];
+
+        /**
+         * printando de duas formas se for um array de pks ou uma só pk
+         **/
+        print $print;
 
     }
 
@@ -324,19 +394,19 @@ class Migration_Database
 
 $ar = [
     "src" => [
-        "user" => '',
-        "host" => '.db.4855800.hostedresource.com',
-        "pass" => '@',
-        "database" => ''
+        "user" => "",
+        "host" => ".db.4855800.hostedresource.com",
+        "pass" => "@",
+        "database" => ""
     ],
     "clone" => [
-        "user" => '',
-        "host" => '',
-        "pass" => '@',
-        "database" => 'pi'
+        "user" => "dev",
+        "host" => "localhost",
+        "pass" => "@",
+        "database" => ""
     ],
 ];
 
 $migdb = new Migration_Database($ar);
 
-$migdb->migrate(['table'=>'qualidade', "pk"=>'', 'where'=>['key'=>'val', 'key2'=>'val2']]);
+$migdb->migrate(["table"=>"qualidade", "pk"=>['pk1', 'pk2'], "where"=>["key"=>"value"]]);
